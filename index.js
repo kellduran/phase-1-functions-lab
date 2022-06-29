2ccff5b0f3aa6f0544386c1d54e6f1fee97ce2f8
Kelly Duran <kaduran16@gmail.com>
2022-06-29 12:52:13 -0400

    commit tree fd6c2cb269227f1fbee9df7d5943b6ee2f4c80f7

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+// Need distance between HQ location and pickup location in blocks. 
+//HQ located on 42nd street, pick-up location is unknown and variable
+
+function distanceFromHqInBlocks(blocks){
+  
+     if (blocks >= 42) {
+        return (blocks - 42);
+    }
+    else {
+        return (42 - blocks)
+    };
+
+}
